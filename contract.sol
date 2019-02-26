581f69216c9937009af11f7f763e3178be4400e3
pkryloff <39795603+pkryloff@users.noreply.github.com>
2019-02-26 17:19:12 +0300

    Update contract.sol

--- contract.sol
+++ contract.sol
@@ -1,12 +1,12 @@
 pragma solidity >=0.4.22 <0.6.0;
 
 contract PaymentSystem{
-    address admin;
-    constructor()  public{
+    address admin; // адрес администратора
+    constructor()  public{ // конструктор контракта 
         admin = msg.sender;
-        countOfAccounts = 0;
+        countOfAccounts = 0; // число счетов при создании контракта 0
     }
-    uint countOfAccounts;
+    uint public countOfAccounts;
     
     struct Account {
         uint accountId; 
@@ -15,9 +15,9 @@ contract PaymentSystem{
         int balance;
     }
     
-    mapping(uint=>Account) public Accounts;
+    mapping(uint=>Account) public Accounts; // маппинг всех счетов, счет по id
     
-    function CreateAcc(int _balance, address _owner, string memory _fullNameOfOwner) public{
+    function CreateAcc(int _balance, address _owner, string memory _fullNameOfOwner) public{ //создание счета
         if(msg.sender==admin){
             Accounts[countOfAccounts].balance = _balance;
             Accounts[countOfAccounts].owner = _owner;
@@ -27,54 +27,13 @@ contract PaymentSystem{
         }
     }
     
-    function ChangeAcc(uint AccountId, int changingValue) public{
-        if(msg.sender==admin){
+    function ChangeAcc(uint AccountId, int changingValue) public{ // изменение баланса счета
+    if(msg.sender==admin){
             Accounts[AccountId].balance += changingValue;
         }
     }
     
-    function Transfer (uint AccountIdFrom, uint AccountIdTo, int Value) public{
-        
-            Accounts[AccountIdFrom].balance -= Value;
-            Accounts[AccountIdTo].balance += Value;
-        }
-    
-}pragma solidity >=0.4.22 <0.6.0;
-
-contract PaymentSystem{
-    address admin;
-    constructor()  public{
-        admin = msg.sender;
-        countOfAccounts = 0;
-    }
-    uint countOfAccounts;
-    
-    struct Account {
-        uint accountId; 
-        address owner;
-        string fullNameOfOwner;
-        int balance;
-    }
-    
-    mapping(uint=>Account) public Accounts;
-    
-    function CreateAcc(int _balance, address _owner, string memory _fullNameOfOwner) public{
-        if(msg.sender==admin){
-            Accounts[countOfAccounts].balance = _balance;
-            Accounts[countOfAccounts].owner = _owner;
-            Accounts[countOfAccounts].fullNameOfOwner = _fullNameOfOwner;
-            Accounts[countOfAccounts].accountId = countOfAccounts;
-            countOfAccounts++;
-        }
-    }
-    
-    function ChangeAcc(uint AccountId, int changingValue) public{
-        if(msg.sender==admin){
-            Accounts[AccountId].balance += changingValue;
-        }
-    }
-    
-    function Transfer (uint AccountIdFrom, uint AccountIdTo, int Value) public{
+    function Transfer (uint AccountIdFrom, uint AccountIdTo, int Value) public{ // перевод с счета на счет
         
             Accounts[AccountIdFrom].balance -= Value;
             Accounts[AccountIdTo].balance += Value;
