aa33f92350c5329ed63e5fffe4fc7bd8d2d3cd35
pkryloff <39795603+pkryloff@users.noreply.github.com>
2019-03-12 17:10:17 +0300

    Update contract.sol
    new functions for admin and overfraft

--- contract.sol
+++ contract.sol
@@ -5,38 +5,75 @@ contract PaymentSystem{
     constructor()  public{ // конструктор контракта 
         admin = msg.sender;
         countOfAccounts = 0; // число счетов при создании контракта 0
+        overdraftLimit = 0;
     }
+    
+     modifier onlyAdmin {
+        require(msg.sender == admin);
+        _;
+    }
+    
     uint public countOfAccounts;
+    
+    int overdraftLimit;
     
     struct Account {
         uint accountId; 
         address owner;
         string fullNameOfOwner;
-        int balance;
+        int balance; //реальный остаток
+        int overdraft;
+        bool isBanned; //забанен ли профиль, по умолчанию false, админ может заблокировать
+        bool isDebtor; //по умолчанию false; true, если не погасил овердрафт до конца дня 
     }
     
     mapping(uint=>Account) public Accounts; // маппинг всех счетов, счет по id
     
-    function CreateAcc(int _balance, address _owner, string memory _fullNameOfOwner) public{ //создание счета
-        if(msg.sender==admin){
+    function SetOverdraftLimit(int newLimit) public{
+        overdraftLimit = newLimit;
+    }
+    
+    function makeUserDebtor(uint AccountId, bool flag) onlyAdmin  public{
+        Accounts[AccountId].isDebtor = flag;
+    }
+    function makeUserBanned(uint AccountId, bool flag) onlyAdmin  public{
+        Accounts[AccountId].isBanned = flag;
+    }
+    
+    function CreateAcc(int _balance, address _owner, string memory _fullNameOfOwner) onlyAdmin public{ //создание счета
             Accounts[countOfAccounts].balance = _balance;
             Accounts[countOfAccounts].owner = _owner;
             Accounts[countOfAccounts].fullNameOfOwner = _fullNameOfOwner;
             Accounts[countOfAccounts].accountId = countOfAccounts;
+            Accounts[countOfAccounts].overdraft = 0;
             countOfAccounts++;
-        }
+    
     }
     
-    function ChangeAcc(uint AccountId, int changingValue) public{ // изменение баланса счета
-    if(msg.sender==admin){
+    function ChangeAcc(uint AccountId, int changingValue) onlyAdmin public{ // изменение баланса счета
             Accounts[AccountId].balance += changingValue;
-        }
     }
     
-    function Transfer (uint AccountIdFrom, uint AccountIdTo, int Value) public{ // перевод с счета на счет
-        
-            Accounts[AccountIdFrom].balance -= Value;
-            Accounts[AccountIdTo].balance += Value;
+    function Transfer (uint AccountIdFrom, uint AccountIdTo, int value) public returns(bool){ // перевод со счета на счет
+            if(Accounts[AccountIdFrom].owner == msg.sender){
+               if(Accounts[AccountIdFrom].balance >= value)
+               {
+                  Accounts[AccountIdFrom].balance -= value;
+                  Accounts[AccountIdTo].balance += value; 
+               }
+               else{
+                   if((Accounts[AccountIdFrom].balance + overdraftLimit - Accounts[AccountIdFrom].overdraft) >= value)
+                   {
+                        Accounts[AccountIdFrom].overdraft += (Accounts[AccountIdFrom].balance - value)*(-1);
+                        Accounts[AccountIdFrom].balance -= value;
+                        Accounts[AccountIdTo].balance += value;
+                   }
+               }
+            }
+            else
+                return false;
         }
-    
+    function repayOverdraft(uint AccountId, int value) public{
+        Accounts[AccountId].overdraft -= value;
+    }
 }
